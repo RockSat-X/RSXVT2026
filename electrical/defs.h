abcfcc941180aaa914028534364eabd9c656b3dd
Phuc X. Doan <phucxdoan@gmail.com>
2025-07-27 23:53:42 -0400

    Cleaned up `primitives.meta`.

--- electrical/defs.h
+++ electrical/defs.h
@@ -51,30 +51,29 @@
 #endif
 
 #include "primitives.meta"
-/* #meta PRIMITIVE_TYPES
-    PRIMITIVE_TYPES = Table(
-        ('name', 'underlying'        , 'size', 'minmax'),
-        ('u8'  , 'unsigned char'     , 1     , True    ),
-        ('u16' , 'unsigned short'    , 2     , True    ),
-        ('u32' , 'unsigned'          , 4     , True    ),
-        ('u64' , 'unsigned long long', 8     , True    ),
-        ('i8'  , 'signed char'       , 1     , True    ),
-        ('i16' , 'signed short'      , 2     , True    ),
-        ('i32' , 'signed'            , 4     , True    ),
-        ('i64' , 'signed long long'  , 8     , True    ),
-        ('b8'  , 'signed char'       , 1     , False   ),
-        ('b16' , 'signed short'      , 2     , False   ),
-        ('b32' , 'signed'            , 4     , False   ),
-        ('b64' , 'signed long long'  , 8     , False   ),
-        ('f32' , 'float'             , 4     , True    ),
-        ('f64' , 'double'            , 8     , True    ),
-    )
+/* #meta
 
-    for type in PRIMITIVE_TYPES:
+    for name, underlying, size in (
+        ('u8'  , 'unsigned char'     , 1),
+        ('u16' , 'unsigned short'    , 2),
+        ('u32' , 'unsigned'          , 4),
+        ('u64' , 'unsigned long long', 8),
+        ('i8'  , 'signed char'       , 1),
+        ('i16' , 'signed short'      , 2),
+        ('i32' , 'signed'            , 4),
+        ('i64' , 'signed long long'  , 8),
+        ('b8'  , 'signed char'       , 1),
+        ('b16' , 'signed short'      , 2),
+        ('b32' , 'signed'            , 4),
+        ('b64' , 'signed long long'  , 8),
+        ('f32' , 'float'             , 4),
+        ('f64' , 'double'            , 8),
+    ):
         Meta.line(f'''
-            typedef {type.underlying} {type.name};
-            static_assert(sizeof({type.name}) == {type.size});
+            typedef {underlying} {name};
+            static_assert(sizeof({name}) == {size});
         ''')
+
 */
 
 //////////////////////////////////////////////////////////////// CMSIS ////////////////////////////////////////////////////////////////
