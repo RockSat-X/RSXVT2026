f230db1d217863fe2887e35923ff36182edf97af
Phuc X. Doan <phucxdoan@gmail.com>
2025-07-28 02:42:59 -0400

    Moved CMSIS stuff into separate system file and making some `#define` macros to do suffix-chopping.

--- electrical/defs.h
+++ electrical/defs.h
@@ -76,35 +76,9 @@
 
 */
 
-//////////////////////////////////////////////////////////////// CMSIS ////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////// Miscellaneous ////////////////////////////////////////////////////////////////
 
-#define _PARENS                                     ()
-#define _EXPAND_0(...)                              _EXPAND_1(_EXPAND_1(_EXPAND_1(__VA_ARGS__)))
-#define _EXPAND_1(...)                              _EXPAND_2(_EXPAND_2(_EXPAND_2(__VA_ARGS__)))
-#define _EXPAND_2(...)                              __VA_ARGS__
-#define _MAP__(F, X, A, B, ...)                     F(X, A, B) __VA_OPT__(_MAP_ _PARENS (F, X, __VA_ARGS__))
-#define _MAP_()                                     _MAP__
-#define _MAP(F, X, ...)                             __VA_OPT__(_EXPAND_0(_MAP__(F, X, __VA_ARGS__)))
-#define _ZEROING(SECT_REG, FIELD, VALUE)            | CONCAT(CONCAT(SECT_REG##_, FIELD##_), Msk)
-#define _SETTING(SECT_REG, FIELD, VALUE)            | (((VALUE) << CONCAT(CONCAT(SECT_REG##_, FIELD##_), Pos)) & CONCAT(CONCAT(SECT_REG##_, FIELD##_), Msk))
-#define CMSIS_READ(SECT_REG, VAR, FIELD)            ((u32) (((VAR) & CONCAT(CONCAT(CONCAT(SECT_REG, _), FIELD##_), Msk)) >> CONCAT(CONCAT(CONCAT(SECT_REG, _), FIELD##_), Pos)))
-#define CMSIS_WRITE(SECT_REG, VAR, FIELD_VALUES...) ((void) ((VAR) = ((VAR) & ~(0 _MAP(_ZEROING, SECT_REG, FIELD_VALUES))) _MAP(_SETTING, SECT_REG, FIELD_VALUES)))
-#define CMSIS_SET(SECT, REG, FIELD_VALUES...)       CMSIS_WRITE(CONCAT(SECT##_, REG), SECT->REG, FIELD_VALUES)
-#define CMSIS_GET(SECT, REG, FIELD)                 CMSIS_READ (CONCAT(SECT##_, REG), SECT->REG, FIELD       )
-#define CMSIS_GET_FROM(VAR, SECT, REG, FIELD)       CMSIS_READ (CONCAT(SECT##_, REG), VAR      , FIELD       )
-
-struct CMSISPutTuple
-{
-    volatile uint32_t* dst;
-    i32                pos;
-    u32                msk;
-};
-
-static mustinline void
-CMSIS_PUT(struct CMSISPutTuple tuple, u32 value)
-{
-    *tuple.dst = ((value) << tuple.pos) & tuple.msk;
-}
+#include "system/cmsis.c"
 
 //////////////////////////////////////////////////////////////// Globals ////////////////////////////////////////////////////////////////
 
