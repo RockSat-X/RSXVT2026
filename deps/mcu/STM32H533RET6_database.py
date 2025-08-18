dba9f629503e7dcc61a29ebfc4294a86175beec8
Phuc X. Doan <phucxdoan@gmail.com>
2025-08-18 01:29:32 -0400

    Factored out common registers.

--- deps/mcu/STM32H533RET6_database.py
+++ deps/mcu/STM32H533RET6_database.py
@@ -22,7 +22,6 @@
             ('analog'   , '0b11'),
         )),
     ),
-
     'minmax' : (
         ('pll_channel_freq',   1_000_000, 250_000_000), # @/pg 124/tbl 47/`H533ds`. TODO We're assuming a high internal voltage and wide range.
         ('pll_vco_freq'    , 128_000_000, 560_000_000), # " TODO Assuming wide frequency range.
@@ -30,7 +29,6 @@
         ('axi_ahb_freq'    ,           0, 250_000_000), # "
         ('apb_freq'        ,           0, 250_000_000), # "
     ),
-
     'registers' : {
         'SysTick' : {
             'LOAD' : {
@@ -86,29 +84,36 @@
                 'HSION'    : 'hsi_enable'  ,
             },
             'CFGR1' : {
-                'SWS' : {
-                    'name'  : 'effective_scgu_clock_source',
-                    'value' : (
-                        ('hsi_ck'   , '0b000'),
-                        ('csi_ck'   , '0b001'),
-                        ('hse_ck'   , '0b010'),
-                        ('pll1_p_ck', '0b011'),
-                    ),
-                },
-                'SW' : {
-                    'name'  : 'scgu_clock_source',
-                    'value' : (
-                        ('hsi_ck'   , '0b000'),
-                        ('csi_ck'   , '0b001'),
-                        ('hse_ck'   , '0b010'),
-                        ('pll1_p_ck', '0b011'),
-                    ),
+                **{
+                    field : {
+                        'name' : name,
+                        'value' : (
+                            ('hsi_ck'   , '0b000'),
+                            ('csi_ck'   , '0b001'),
+                            ('hse_ck'   , '0b010'),
+                            ('pll1_p_ck', '0b011'),
+                        ),
+                    }
+                    for field, name in (
+                        ('SWS', 'effective_scgu_clock_source'),
+                        ('SW' , 'scgu_clock_source'          ),
+                    )
                 },
             },
             'CFGR2' : {
-                'PPRE3' : { 'name' : 'apb3_divider', 'value' : ((1, '0b000'), (2, '0b100'), (4, '0b101'), (8, '0b110'), (16, '0b111')) },
-                'PPRE2' : { 'name' : 'apb2_divider', 'value' : ((1, '0b000'), (2, '0b100'), (4, '0b101'), (8, '0b110'), (16, '0b111')) },
-                'PPRE1' : { 'name' : 'apb1_divider', 'value' : ((1, '0b000'), (2, '0b100'), (4, '0b101'), (8, '0b110'), (16, '0b111')) },
+                **{
+                    f'PPRE{unit}' : {
+                        'name'  : f'apb{unit}_divider',
+                        'value' : (
+                            (1 , '0b000'),
+                            (2 , '0b100'),
+                            (4 , '0b101'),
+                            (8 , '0b110'),
+                            (16, '0b111'),
+                        ),
+                    }
+                    for unit in (1, 2, 3)
+                },
                 'HPRE'  : {
                     'name'  : 'cpu_divider',
                     'value' : (
@@ -124,147 +129,95 @@
                     ),
                 },
             },
-            'PLL1CFGR' : {
-                'PLL1REN' : 'pll1r_enable',
-                'PLL1QEN' : 'pll1q_enable',
-                'PLL1PEN' : 'pll1p_enable',
-                'PLL1M'   : {
-                    'name'   : 'pll1_predivider',
-                    'minmax' : (1, 63),
-                },
-                'PLL1SRC' : {
-                    'name'  : 'pll1_clock_source',
-                    'value' : (
-                        (None    , '0b00'),
-                        ('hsi_ck', '0b01'),
-                        ('csi_ck', '0b10'),
-                        ('hse_ck', '0b11'),
-                    ),
-                },
-                'PLL1RGE' : {
-                    'name'  : 'pll1_input_range',
-                    'value' : (
-                        ( 1_000_000, None),
-                        ( 2_000_000, None), # TODO Can be '0b00', but only for medium VCO. @/pg 124/tbl 47/`H533rm`.
-                        ( 4_000_000, 0b01),
-                        ( 8_000_000, 0b10),
-                        (16_000_000, 0b11),
-                    ),
-                },
+            **{
+                f'PLL{unit}CFGR' : {
+                    f'PLL{unit}REN' : f'pll{unit}r_enable',
+                    f'PLL{unit}QEN' : f'pll{unit}q_enable',
+                    f'PLL{unit}PEN' : f'pll{unit}p_enable',
+                    f'PLL{unit}M'   : {
+                        'name'   : f'pll{unit}_predivider',
+                        'minmax' : (1, 63),
+                    },
+                    f'PLL{unit}SRC' : {
+                        'name'  : f'pll{unit}_clock_source',
+                        'value' : (
+                            (None    , '0b00'),
+                            ('hsi_ck', '0b01'),
+                            ('csi_ck', '0b10'),
+                            ('hse_ck', '0b11'),
+                        ),
+                    },
+                    f'PLL{unit}RGE' : {
+                        'name'  : f'pll{unit}_input_range',
+                        'value' : (
+                            ( 1_000_000, None),
+                            ( 2_000_000, None), # TODO Can be '0b00', but only for medium VCO. @/pg 124/tbl 47/`H533rm`.
+                            ( 4_000_000, 0b01),
+                            ( 8_000_000, 0b10),
+                            (16_000_000, 0b11),
+                        ),
+                    },
+                }
+                for unit in (1, 2, 3)
             },
-            'PLL2CFGR' : {
-                'PLL2REN' : 'pll2r_enable',
-                'PLL2QEN' : 'pll2q_enable',
-                'PLL2PEN' : 'pll2p_enable',
-                'PLL2M'   : {
-                    'name'   : 'pll2_predivider',
-                    'minmax' : (1, 63),
-                },
-                'PLL2SRC' : {
-                    'name'  : 'pll2_clock_source',
-                    'value' : (
-                        (None    , '0b00'),
-                        ('hsi_ck', '0b01'),
-                        ('csi_ck', '0b10'),
-                        ('hse_ck', '0b11'),
-                    ),
-                },
-                'PLL2RGE' : {
-                    'name'  : 'pll2_input_range',
-                    'value' : (
-                        ( 1_000_000, None),
-                        ( 2_000_000, None), # TODO Can be '0b00', but only for medium VCO. @/pg 124/tbl 47/`H533rm`.
-                        ( 4_000_000, 0b01),
-                        ( 8_000_000, 0b10),
-                        (16_000_000, 0b11),
-                    ),
-                },
-            },
-            'PLL3CFGR' : {
-                'PLL3REN' : 'pll3r_enable',
-                'PLL3QEN' : 'pll3q_enable',
-                'PLL3PEN' : 'pll3p_enable',
-                'PLL3M'   : {
-                    'name'   : 'pll3_predivider',
-                    'minmax' : (1, 63),
-                },
-                'PLL3SRC' : {
-                    'name'  : 'pll3_clock_source',
-                    'value' : (
-                        (None    , '0b00'),
-                        ('hsi_ck', '0b01'),
-                        ('csi_ck', '0b10'),
-                        ('hse_ck', '0b11'),
-                    ),
-                },
-                'PLL3RGE' : {
-                    'name'  : 'pll3_input_range',
-                    'value' : (
-                        ( 1_000_000, None),
-                        ( 2_000_000, None), # TODO Can be '0b00', but only for medium VCO. @/pg 124/tbl 47/`H533rm`.
-                        ( 4_000_000, 0b01),
-                        ( 8_000_000, 0b10),
-                        (16_000_000, 0b11),
-                    ),
-                },
-            },
-            'PLL1DIVR' : {
-                'PLL1R' : { 'name' : 'pll1r_divider'  , 'minmax' : (1, 128) },
-                'PLL1Q' : { 'name' : 'pll1q_divider'  , 'minmax' : (1, 128) },
-                'PLL1P' : { 'name' : 'pll1p_divider'  , 'minmax' : (1, 128) },
-                'PLL1N' : { 'name' : 'pll1_multiplier', 'minmax' : (4, 512) },
-            },
-            'PLL2DIVR' : {
-                'PLL2R' : { 'name' : 'pll2r_divider'  , 'minmax' : (1, 128) },
-                'PLL2Q' : { 'name' : 'pll2q_divider'  , 'minmax' : (1, 128) },
-                'PLL2P' : { 'name' : 'pll2p_divider'  , 'minmax' : (1, 128) },
-                'PLL2N' : { 'name' : 'pll2_multiplier', 'minmax' : (4, 512) },
-            },
-            'PLL3DIVR' : {
-                'PLL3R' : { 'name' : 'pll3r_divider'  , 'minmax' : (1, 128) },
-                'PLL3Q' : { 'name' : 'pll3q_divider'  , 'minmax' : (1, 128) },
-                'PLL3P' : { 'name' : 'pll3p_divider'  , 'minmax' : (1, 128) },
-                'PLL3N' : { 'name' : 'pll3_multiplier', 'minmax' : (4, 512) },
+            **{
+                f'PLL{unit}DIVR' : {
+                    f'PLL{unit}R' : { 'name' : f'pll{unit}r_divider'  , 'minmax' : (1, 128) },
+                    f'PLL{unit}Q' : { 'name' : f'pll{unit}q_divider'  , 'minmax' : (1, 128) },
+                    f'PLL{unit}P' : { 'name' : f'pll{unit}p_divider'  , 'minmax' : (1, 128) },
+                    f'PLL{unit}N' : { 'name' : f'pll{unit}_multiplier', 'minmax' : (4, 512) },
+                }
+                for unit in (1, 2, 3)
             },
             'APB1LENR' : {
                 'USART2EN' : 'uxart_2_enable',
             },
             'CCIPR1' : {
-                'USART2SEL' : {
-                    'name'  : f'uxart_{(('usart', 2),)}_clock_source',
-                    'value' : (
-                        ('apb1_ck'   , '0b000'),
-                        ('pll2_q_ck' , '0b001'),
-                        ('pll3_q_ck' , '0b010'),
-                        ('hsi_ker_ck', '0b011'),
-                        ('csi_ker_ck', '0b100'),
-                        (None        , '0b110'),
-                        # TODO: ('lse_ck', '0b101'),
-                    ),
-                },
-                'USART3SEL' : {
-                    'name'  : f'uxart_{(('usart', 3),)}_clock_source',
-                    'value' : (
-                        ('apb1_ck'   , '0b000'),
-                        ('pll2_q_ck' , '0b001'),
-                        ('pll3_q_ck' , '0b010'),
-                        ('hsi_ker_ck', '0b011'),
-                        ('csi_ker_ck', '0b100'),
-                        (None        , '0b110'),
-                        # TODO: ('lse_ck', '0b101'),
-                    ),
-                },
-                'USART6SEL' : {
-                    'name'  : f'uxart_{(('usart', 6),)}_clock_source',
-                    'value' : (
-                        ('apb1_ck'   , '0b000'),
-                        ('pll2_q_ck' , '0b001'),
-                        ('pll3_q_ck' , '0b010'),
-                        ('hsi_ker_ck', '0b011'),
-                        ('csi_ker_ck', '0b100'),
-                        (None        , '0b110'),
-                        # TODO: ('lse_ck', '0b101'),
-                    ),
+                **{
+                    field : {
+                        'name'  : f'uxart_{peripherals}_clock_source',
+                        'value' : clock_source
+                    }
+                    for field_peripherals, clock_source in
+                    (
+                        (
+                            (
+                                ('USART10SEL', (('usart', 10),)),
+                                ('UART9SEL'  , (('uart' , 9 ),)),
+                                ('UART8SEL'  , (('uart' , 8 ),)),
+                                ('UART7SEL'  , (('uart' , 7 ),)),
+                                ('USART6SEL' , (('usart', 6 ),)),
+                                ('UART5SEL'  , (('uart' , 5 ),)),
+                                ('UART4SEL'  , (('uart' , 4 ),)),
+                                ('USART3SEL' , (('usart', 3 ),)),
+                                ('USART2SEL' , (('usart', 2 ),)),
+                            ),
+                            (
+                                ('apb1_ck'   , '0b000'),
+                                ('pll2_q_ck' , '0b001'),
+                                ('pll3_q_ck' , '0b010'),
+                                ('hsi_ker_ck', '0b011'),
+                                ('csi_ker_ck', '0b100'),
+                                # TODO: ('lse_ck', '0b101'),
+                                (None        , '0b110'),
+                            ),
+                        ),
+                        (
+                            (
+                                ('USART1SEL', (('usart', 1),)),
+                            ),
+                            (
+                                ('rcc_pclk2' , '0b000'),
+                                ('pll2_q_ck' , '0b001'),
+                                ('pll3_q_ck' , '0b010'),
+                                ('hsi_ker_ck', '0b011'),
+                                ('csi_ker_ck', '0b100'),
+                                # TODO: ('lse_ck'    , '0b101'),
+                                (None        , '0b110'),
+                            ),
+                        ),
+                    )
+                    for field, peripherals in field_peripherals
                 },
             },
             'CCIPR5' : {
