85ce0eb54525ea8c055a9ce4a2355c9e3779a9db
Phuc X. Doan <phucxdoan@gmail.com>
2025-08-18 03:11:13 -0400

    Ported the STM32H7S3L8H6 database to Python expression.

--- electrical/system/database.py
+++ electrical/system/database.py
@@ -9,12 +9,7 @@ from deps.pxd.sexp import parse_sexp
 
 
 
-# Helper routine to make sure all fields in a tag are supplied.
-# e.g:
-# >
-# >    ('pll{UNIT}{CHANNEL}_enable', { 'CHANNEL' : 'q', 'UNIT' : 3 })
-# >         ^^^^^^^^^^^^^^^----------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# >
+# TODO.
 
 def verify_and_get_field_names_in_tag_order(tag, given_fields):
 
@@ -46,244 +41,61 @@ for mcu in MCUS:
 
     SYSTEM_DATABASE[mcu] = {}
 
-
-
-    # TODO.
-
-    if mcu == 'STM32H533RET6':
-
-        database_file_path = root(f'./deps/mcu/{mcu}_database.py')
-
-        if not database_file_path.is_file():
-            raise RuntimeError(
-                f'File "{database_file_path}" does not exist; '
-                f'it is needed to describe the properties of the MCU.'
-            )
-
-        database_py = eval(database_file_path.read_text(), {}, {})
-
-        for name, value in database_py['value']:
-            SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
-                value = value,
-            )
-
-        for name, minimum, maximum in database_py['minmax']:
-            SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
-                minimum = minimum,
-                maximum = maximum,
-            )
-
-        for section, *registers in database_py['registers']:
-            for register, *fields in registers:
-                for entry in fields:
-
-                    match entry:
-
-                        case (field, name, minimum, maximum):
-                            SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
-                                section  = section,
-                                register = register,
-                                field    = field,
-                                minimum  = minimum,
-                                maximum  = maximum,
-                            )
-
-                        case (field, name, value):
-                            SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
-                                section  = section,
-                                register = register,
-                                field    = field,
-                                value    = value,
-                            )
-
-                        case (field, name):
-                            SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
-                                section  = section,
-                                register = register,
-                                field    = field,
-                                value    = (False, True),
-                            )
-
-                        case unknown:
-                            raise ValueError(f'Unknown value: {repr(unknown)}.')
-
-        continue
-
-
-
-    # The MCU database is defined as an S-expression
-    # in a separate text file that we parse.
-
-    database_file_path = root(f'./deps/mcu/{mcu}_database.txt')
+    database_file_path = root(f'./deps/mcu/{mcu}_database.py')
 
     if not database_file_path.is_file():
         raise RuntimeError(
             f'File "{database_file_path}" does not exist; '
-            f'a file of S-expressions is needed to describe '
-            f'the properties of the MCU.'
+            f'it is needed to describe the properties of the MCU.'
         )
 
+    database_py = eval(database_file_path.read_text(), {}, {})
+
+    for name, value in database_py['value']:
+        SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
+            value = value,
+        )
+
+    for name, minimum, maximum in database_py['minmax']:
+        SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
+            minimum = minimum,
+            maximum = maximum,
+        )
+
+    for section, *registers in database_py['registers']:
+        for register, *fields in registers:
+            for entry in fields:
+
+                match entry:
+
+                    case (field, name, minimum, maximum):
+                        SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
+                            section  = section,
+                            register = register,
+                            field    = field,
+                            minimum  = minimum,
+                            maximum  = maximum,
+                        )
+
+                    case (field, name, value):
+                        SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
+                            section  = section,
+                            register = register,
+                            field    = field,
+                            value    = value,
+                        )
+
+                    case (field, name):
+                        SYSTEM_DATABASE[mcu][name] = types.SimpleNamespace(
+                            section  = section,
+                            register = register,
+                            field    = field,
+                            value    = (False, True),
+                        )
+
+                    case unknown:
+                        raise ValueError(f'Unknown value: {repr(unknown)}.')
 
-
-
-    for sexp in parse_sexp(database_file_path.read_text()):
-
-
-
-        # Get the tag and attributes.
-        # e.g:
-        # >
-        # >    (pll{UNIT}_predivider (RCC PLL2CFGR PLL2M) (UNIT : 2) (.minmax 1 63))
-        # >     ^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        # >     Tag.                 Attributes.
-        # >
-
-        tag, *attributes = sexp
-        entry            = AllocatingNamespace()
-
-
-
-        # Look for the dotted attribute which will represent the value of the entry.
-        # e.g:
-        # >
-        # >    (pll{UNIT}_predivider (RCC PLL2CFGR PLL2M) (UNIT : 2) (.minmax 1 63))
-        # >                                                          ^^^^^^^^^^^^^^
-        # >
-
-        for attribute_i, attribute in enumerate(attributes):
-
-            match attribute:
-
-
-
-                # The entry value is directly given.
-                # e.g:
-                # >
-                # >    (APB_UNITS (.value (1 2 4 5)))
-                # >               ^^^^^^^^^^^^^^^^^^
-                # >
-
-                case ('.value', value):
-                    entry.value = value
-
-
-
-                # The entry value is an inclusive range.
-                # e.g:
-                # >
-                # >    (flash_latency (FLASH ACR LATENCY) (.minmax 0b0000 0b1111))
-                # >                                       ^^^^^^^^^^^^^^^^^^^^^^^
-                # >
-
-                case ('.minmax', minimum, maximum):
-                    entry.minimum = minimum
-                    entry.maximum = maximum
-
-
-
-                # This entry attribute is something else which we'll process later.
-
-                case _: continue
-
-
-
-            # We found and processed the entry value; no need to process it again.
-
-            del attributes[attribute_i]
-            break
-
-
-
-        # No dotted entry value found, so we assume it's just
-        # something like a True/False 1-bit register.
-        # e.g:
-        # >
-        # >    (current_active_vos_ready (PWR SR1 ACTVOSRDY))
-        # >
-
-        else:
-            entry.value = (False, True)
-
-
-
-        # We then look for field-value attributes.
-        # e.g:
-        # >
-        # >    (pll{UNIT}_ready (RCC CR PLL3RDY) (UNIT : 3))
-        # >         ^^^^-------------------------^^^^^^^^^^
-        # >
-
-        attribute_i = 0
-
-        while attribute_i < len(attributes):
-
-            match attribute := attributes[attribute_i]:
-
-                case (attribute_name, ':', attribute_value):
-                    entry[attribute_name] = attribute_value
-
-                case _:
-                    attribute_i += 1
-                    continue
-
-            del attributes[attribute_i]
-
-
-
-        # Process the remaining attributes.
-
-        match attributes:
-
-
-
-            # Nothing else to do.
-
-            case []:
-                pass
-
-
-
-            # The entry is also provided with a section-register-field location.
-            # e.g:
-            # >
-            # >    (current_active_vos_ready (PWR SR1 ACTVOSRDY))
-            # >                              ^^^^^^^^^^^^^^^^^^^
-            # >
-
-            case [(section, register, field)]:
-                entry.section  = section
-                entry.register = register
-                entry.field    = field
-
-
-
-            # Leftover attributes.
-
-            case _:
-                raise ValueError(f'Leftover attributes: {repr(attributes)}.')
-
-
-
-        # Determine the fields in the entry's tag.
-        # e.g:
-        # >
-        # >    (pll{UNIT}{CHANNEL}_enable (RCC PLLCFGR PLL1PEN) (UNIT : 1) (CHANNEL : p))
-        # >         ^^^^  ^^^^^^^
-        # >
-
-        field_names = OrderedSet(re.findall('{(.*?)}', tag))
-
-
-
-        # TODO Prevent entries from being overridden.
-
-
-
-        # TODO.
-
-        fields                             = { name : entry[name] for name in field_names }
-        expanded_tag                       = tag.format(**fields)
-        SYSTEM_DATABASE[mcu][expanded_tag] = types.SimpleNamespace(**entry.__dict__)
 
 
 
@@ -291,36 +103,4 @@ for mcu in MCUS:
 
 
 
-# This meta-directive creates what I call the "database" for a microcontroller.
-# The so-called database simply contains information from the MCU's reference
-# manual and datasheet.
-#
-# For instance, things like:
-#
-#     - the maximum bus frequency,
-#     - the available clock sources for a particular PLL unit,
-#     - the range of predividers for making the baud of a UART peripheral,
-#     - the register to enable an internal oscillator,
-#     - etc.
-#
-# The database is extremely useful for porting across different microcontrollers
-# because, first, obviously the specifications values will be different,
-# but more importantly, the naming convention is different.
-#
-# One reference manual might use the field name "PLL3ON" to enable PLL3,
-# while a different RM might use the field name "PLL3EN" instead.
-# To account for this, the database organizes everything by "tags",
-# so rather than use names like "PLL3ON" or "PLL3EN", we instead use the
-# more human readable key of "pll_{UNIT}_enable".
-# The field "{UNIT}" would be eventually replaced with 3 later on,
-# so this makes using the tag "pll_{UNIT}_enable" very natural because we often
-# iterate over the other PLL units too.
-#
-# This layer of abstraction makes it very easy to read/modify registers
-# across multiple different microcontroller models without getting bogged
-# down by the different values and naming conventions.
-#
-# The way the database is defined is in "deps/mcu/<MCU>_database.txt" which
-# is just a S-expression file. I'm not going to go into detail how the syntax
-# exactly works here since I think it's pretty self-explanatory.
-# If you're curious just read the Python code above and play with it!
+# TODO.
