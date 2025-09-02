dd42636c2e7f8b9a61432b371a5607c085a356ae
Phuc X. Doan <phucxdoan@gmail.com>
2025-09-02 14:37:41 -0400

    Added delay and comments.

--- electrical/I2CDriverTest.c
+++ electrical/I2CDriverTest.c
@@ -5,13 +5,32 @@
 extern noret void
 main(void)
 {
+
     SYSTEM_init();
     JIG_init();
+
+
+
+    // A delay here so if we inspect the I2C pins,
+    // it's clearly differentiated from when the MCU
+    // is under reset.
+
+    spinlock_nop(100'000'000);
+
+
+
+    // Set up the I2C.
+
     I2C_reinit();
+
+
+
+    // TODO Test the I2C.
 
     for (;;)
     {
         GPIO_TOGGLE(led_green);
         spinlock_nop(10'000'000);
     }
+
 }
