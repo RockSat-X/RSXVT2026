f69cbee0a0fc022fdbdf9a82e4b7cbd936364a80
Phuc X. Doan <phucxdoan@gmail.com>
2025-09-20 09:22:41 -0400

    Incompletion message.

--- electrical/DemoSPI.c
+++ electrical/DemoSPI.c
@@ -19,6 +19,11 @@ main(void)
 
     ////////////////////////////////////////////////////////////////////////////////
 
-    sorry
+    for (;;)
+    {
+        stlink_tx("Sorry! The SPI driver is to be implemented. Come back later!\n");
+        GPIO_TOGGLE(led_green);
+        spinlock_nop(100'000'000);
+    }
 
 }
