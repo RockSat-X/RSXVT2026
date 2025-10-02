4853fdf3dd6ead6709ef58544c7b9451a71de283
Phuc X. Doan <phucxdoan@gmail.com>
2025-10-02 19:51:37 -0400

    Did bare-minimum to get FatFs working with SD driver.

--- electrical/filesystem.c
+++ electrical/filesystem.c
@@ -1,3 +1,5 @@
+// TODO Ensure there's an SD driver?
+
 #define FFCONF_DEF         80386
 #define FF_FS_READONLY     0
 #define FF_FS_MINIMIZE     0
@@ -51,13 +53,62 @@
 
 
 extern DSTATUS
+disk_initialize(BYTE pdrv)
+{
+
+    if (pdrv != 0)
+        sorry
+
+    while (_SD_drivers[0].state == SDDriverState_initer); // TODO What if no SD_reinit?
+
+    switch (_SD_drivers[0].state)
+    {
+        case SDDriverState_active:
+        {
+            return 0;
+        } break;
+
+        case SDDriverState_error:
+        {
+            return STA_NOINIT;
+        } break;
+
+        case SDDriverState_initer : sorry
+        default                   : sorry
+    }
+
+}
+
+
+
+extern DSTATUS
 disk_status(BYTE pdrv)
 {
 
     if (pdrv != 0)
-        panic;
+        sorry
 
-    sorry
+    switch (_SD_drivers[0].state)
+    {
+
+        case SDDriverState_initer:
+        {
+            sorry
+        } break;
+
+        case SDDriverState_active:
+        {
+            return 0;
+        } break;
+
+        case SDDriverState_error:
+        {
+            sorry
+        } break;
+
+        default: sorry;
+
+    }
 
 }
 
@@ -68,9 +119,51 @@ disk_write(BYTE pdrv, const BYTE* buff, LBA_t sector, UINT count)
 {
 
     if (pdrv != 0)
-        panic;
+        sorry
 
-    sorry
+    for (UINT index = 0; index < count; index += 1)
+    {
+        enum SDDo do_result = {0};
+
+        do
+        {
+            do_result =
+                SD_do
+                (
+                    0,
+                    SDOperation_single_write,
+                    &((struct Sector*) buff)[index],
+                    sector + index
+                );
+        }
+        while (do_result == SDDo_task_in_progress);
+
+        switch (do_result)
+        {
+
+            case SDDo_task_completed:
+            {
+                // The sector-write was a success!
+            } break;
+
+            case SDDo_task_error:
+            {
+                sorry
+            } break;
+
+            case SDDo_driver_error:
+            {
+                sorry
+            } break;
+
+            case SDDo_task_in_progress : sorry
+            case SDDo_bug              : sorry
+            default                    : sorry
+        }
+
+    }
+
+    return RES_OK;
 
 }
 
@@ -81,9 +174,51 @@ disk_read(BYTE pdrv, BYTE* buff, LBA_t sector, UINT count)
 {
 
     if (pdrv != 0)
-        panic;
+        sorry
 
-    sorry
+    for (UINT index = 0; index < count; index += 1)
+    {
+        enum SDDo do_result = {0};
+
+        do
+        {
+            do_result =
+                SD_do
+                (
+                    0,
+                    SDOperation_single_read,
+                    &((struct Sector*) buff)[index],
+                    sector + index
+                );
+        }
+        while (do_result == SDDo_task_in_progress);
+
+        switch (do_result)
+        {
+
+            case SDDo_task_completed:
+            {
+                // The sector-read was a success!
+            } break;
+
+            case SDDo_task_error:
+            {
+                sorry
+            } break;
+
+            case SDDo_driver_error:
+            {
+                sorry
+            } break;
+
+            case SDDo_task_in_progress : sorry
+            case SDDo_bug              : sorry
+            default                    : sorry
+        }
+
+    }
+
+    return RES_OK;
 
 }
 
@@ -94,29 +229,88 @@ disk_ioctl(BYTE pdrv, BYTE cmd, void* buff)
 {
 
     if (pdrv != 0)
-        panic;
+        sorry
 
-    sorry
+    switch (cmd)
+    {
+
+        case CTRL_SYNC:
+        {
+            return RES_OK; // TODO Document.
+        } break;
+
+        case GET_SECTOR_COUNT:
+        {
+            sorry
+        } break;
+
+        case GET_SECTOR_SIZE:
+        {
+            sorry
+        } break;
+
+        case GET_BLOCK_SIZE:
+        {
+            sorry
+        } break;
+
+        case CTRL_TRIM:
+        {
+            sorry
+        } break;
+
+        default:
+        {
+            sorry
+        } break;
+
+    }
 
 }
 
 
 
-extern DSTATUS
-disk_initialize(BYTE pdrv)
+static void
+_FILESYSTEM_fctprintf_callback(char character, void* void_fp)
 {
 
-    if (pdrv != 0)
-        panic;
+    UINT bytes_written = {0};
 
-    sorry
+    FRESULT result =
+        f_write
+        (
+            (FIL*) void_fp,
+            &character,
+            sizeof(character),
+            &bytes_written
+        );
+
+    if (result != FR_OK)
+    {
+        sorry
+    }
 
 }
 
 
 
-extern int
+extern int __attribute__((format(printf, 2, 3)))
 f_printf(FIL* fp, const TCHAR* str, ...)
 {
-    sorry
+
+    va_list arguments = {0};
+    va_start(arguments);
+
+    int result = vfctprintf // TODO Define `vfctprintf` in `system.h`?
+    (
+        &_FILESYSTEM_fctprintf_callback,
+        fp,
+        str,
+        arguments
+    );
+
+    va_end(arguments);
+
+    return result;
+
 }
