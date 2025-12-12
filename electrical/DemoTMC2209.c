31704ea74185f5ac34c205851161e762a98eb825
Phuc X. Doan <phucxdoan@gmail.com>
2025-12-11 19:43:57 -0500

    Cleaned up and documented timer stepping sequence.

--- electrical/DemoTMC2209.c
+++ electrical/DemoTMC2209.c
@@ -117,44 +117,150 @@ main(void)
 
     GPIO_LOW(driver_enable);
 
-    #include "DemoTMC2209_POSITIONS.meta"
-    /* #meta
+    #define STEP_PERIOD_US 25'000
 
-        import math
-
-        with Meta.enter(f'static const i32 POSITIONS[] ='):
-            for i in range(4000):
-                Meta.line(f'''
-                    {round(math.sin(i / 4000 * 2 * math.pi * 16) * 1500)},
-                ''')
-
-    */
-
-    i32 index = 0;
+    static i8  step_buffer_data[32] = {0};
+    static u32 step_buffer_reader   = 0;
+    static u32 step_buffer_writer   = 0;
 
     for (;;)
     {
 
-        while (!CMSIS_GET(TIM15, SR, UIF));
-        CMSIS_SET(TIM15, SR, UIF, false);
+        // Step queuing.
 
-        i32 now = POSITIONS[index];
+        {
 
-        index += 1;
-        index %= countof(POSITIONS);
+            // See if we can queue up the
+            // next sequence of steps to take.
 
-        i32 next = POSITIONS[index];
+            if (step_buffer_writer - step_buffer_reader < countof(step_buffer_data))
+            {
 
-        i32 delta = next - now;
+                // Compute the steps that'll need to be taken.
 
-        delta = delta < 0 ? -delta : delta;
+                static i32 index = 0;
 
-        GPIO_SET(driver_direction, next > now);
-        CMSIS_SET(TIM15, CCR1, CCR1, delta ?         1   : -1  );
-        CMSIS_SET(TIM15, RCR , REP , delta ? delta - 1 : 0     );
-        CMSIS_SET(TIM15, ARR , ARR , (10'000 - 1) / (delta + 1));
-        CMSIS_SET(TIM15, EGR , UG  , true                      );
-        CMSIS_SET(TIM15, CR1 , CEN , true                      );
+                #include "DemoTMC2209_STEPS.meta"
+                /* #meta
+
+                    import math
+
+                    with Meta.enter(f'static const i8 STEPS[] ='):
+                        for i in range(4000):
+                            Meta.line(f'''
+                                {round(math.sin(i / 4000 * 2 * math.pi * 16) * 127)},
+                            ''')
+
+                */
+
+                i8 steps = STEPS[index];
+
+                index += 1;
+                index %= countof(STEPS);
+
+
+                // Push into the ring-buffer.
+
+                step_buffer_data[step_buffer_writer % countof(step_buffer_data)]  = steps;
+                step_buffer_writer                                               += 1;
+
+            }
+
+        }
+
+
+
+        // Stepper motor driver stepping.
+
+        {
+
+            // See if the timer has finished pulsing steps;
+            // if so, the next sequence of steps can be queued up now.
+
+            if (CMSIS_GET(TIM15, SR, UIF))
+            {
+
+                CMSIS_SET(TIM15, SR, UIF, false); // Acknowledge timer's update flag.
+
+
+
+                // Determine the amount of steps we'll
+                // need to take now and the direction.
+
+                i32 steps = {0};
+
+                if (step_buffer_reader == step_buffer_writer)
+                {
+                    // Underflow condition.
+                    // The step ring-buffer was exhausted and
+                    // no steps will be taken during this period.
+                    // TODO Indicate this situation somehow.
+                    steps = 0;
+                }
+                else
+                {
+                    // Pop from the ring-buffer the next
+                    // signed amount of steps to take.
+                    steps               = step_buffer_data[step_buffer_reader % countof(step_buffer_data)];
+                    step_buffer_reader += 1;
+                }
+
+                i32 abs_steps = steps < 0 ? -steps : steps;
+
+
+
+                // Set the direction arbitrarily.
+
+                GPIO_SET(driver_direction, steps > 0);
+
+
+
+                // Set the amount of step pulses to be done.
+                // Note that this field has it so a value of
+                // 0 encodes a repetition of 1, so in the event
+                // of no steps to be done, there still needs to
+                // be a repetition.
+
+                CMSIS_SET(TIM15, RCR, REP, abs_steps ? (abs_steps - 1) : 0);
+
+
+
+                // So to prevent a pulse in the case of zero steps,
+                // the comparison value is changed to the largest
+                // value so the pulse won't occur.
+                // In any other case, the comparison value is non-zero
+                // so that the waveform has its rising edge slighly
+                // delayed from when the DIR signal is updated.
+
+                CMSIS_SET(TIM15, CCR1, CCR1, abs_steps ? 1 : -1);
+
+
+
+                // The amount of time between each step pulse
+                // is set so that after N steps, a fixed amount
+                // of time has passed. This allows us to update
+                // the direction and rate of stepping at fixed
+                // intervals.
+
+                CMSIS_SET(TIM15, ARR, ARR, (STEP_PERIOD_US - 1) / (abs_steps ? abs_steps : 1));
+
+
+
+                // Generate an update event so the above configuration
+                // will be loaded into the timer's shadow registers.
+
+                CMSIS_SET(TIM15, EGR, UG, true);
+
+
+
+                // Enable the timer's counter; the counter will
+                // become disabled after the end of step repetitions.
+
+                CMSIS_SET(TIM15, CR1, CEN, true);
+
+            }
+
+        }
 
     }
 
