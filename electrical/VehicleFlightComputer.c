f480c5dd8b9db3b82882e094678999a200001e2c
Phuc X. Doan <phucxdoan@gmail.com>
2026-01-23 18:50:32 -0500

    Removed `DemoStepper` target for now; might add back later.

--- electrical/VehicleFlightComputer.c
+++ electrical/VehicleFlightComputer.c
@@ -203,7 +203,53 @@ main(void)
 
         for (;;)
         {
-            #include "DemoStepper_STEPS.meta"
+            #include "VehicleFlightComputer_STEPS.meta"
+            /* #meta
+
+                import math
+
+                STEPS_PER_REVOLUTION = 1600
+
+                with Meta.enter(f'static const i8 STEPS[] ='):
+
+
+
+                    # Sequence of complicated movements...
+
+                    pattern = [
+                        round(math.sin(math.sin(i / 500 * 2 * math.pi) * i / 125 * 2 * math.pi * 16) * 127)
+                        for i in range(500)
+                    ]
+
+                    for step in pattern:
+                        Meta.line(f'''
+                            {step},
+                        ''')
+
+
+
+                    # Home back to original position to see if any steps were lost.
+
+                    for i in range(-sum(pattern) % STEPS_PER_REVOLUTION // 100):
+                        Meta.line(f'''
+                            100,
+                        ''')
+
+                    for i in range(-sum(pattern) % STEPS_PER_REVOLUTION % 100):
+                        Meta.line(f'''
+                            1,
+                        ''')
+
+
+
+                    # Pause...
+
+                    for i in range(100):
+                        Meta.line(f'''
+                            0,
+                        ''')
+
+            */
 
             static i32 index = 0;
 
