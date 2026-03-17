1d2cdee699b12c0c047dafe0af2b3b184c12dee7
Phuc X. Doan <phucxdoan@gmail.com>
2026-03-17 09:34:40 -0400

    Setting up logger for `DebugBoard`.

--- electrical/DebugBoard.c
+++ electrical/DebugBoard.c
@@ -291,6 +291,257 @@ FREERTOS_TASK(display, 0)
 
 
 
+FREERTOS_TASK(logger, 0)
+{
+
+    static struct Sector working_sectors[2]         = {0};
+    b32                  completely_wipe_filesystem = false;
+
+    for (;;)
+    {
+
+        REINITIALIZE:;
+
+
+
+        // If need be, we can format the SD card. Data saved on the debug
+        // board SD card is not critical as it's only for diagnostic purposes
+        // for things like sequence testing.
+
+        if (completely_wipe_filesystem)
+        {
+            BUZZER_play(BuzzerTune_tetris);
+            GPIO_INACTIVE(led_channel_red_D  );
+            GPIO_INACTIVE(led_channel_green_D);
+            GPIO_INACTIVE(led_channel_blue_D );
+        }
+        else
+        {
+            GPIO_ACTIVE  (led_channel_red_D  );
+            GPIO_INACTIVE(led_channel_green_D);
+            GPIO_ACTIVE  (led_channel_blue_D );
+        }
+
+
+
+        // Try setting up the file-system.
+
+        enum FileSystemReinitResult reinit_result =
+            FILESYSTEM_reinit
+            (
+                SDHandle_primary,
+                working_sectors,
+                completely_wipe_filesystem ? countof(working_sectors) : 0
+            );
+
+        completely_wipe_filesystem = false;
+
+        switch (reinit_result)
+        {
+
+
+
+            case FileSystemReinitResult_success:
+            {
+                // Ready to go!
+            } break;
+
+
+
+            // Couldn't successfully communicate with the SD card,
+            // likely because of poor connection or there's no card mounted.
+            // Either way, we indicate this as a soft error.
+
+            case FileSystemReinitResult_couldnt_ready_card:
+            case FileSystemReinitResult_transfer_error:
+            {
+
+                GPIO_INACTIVE(led_channel_red_D  );
+                GPIO_INACTIVE(led_channel_green_D);
+                GPIO_INACTIVE(led_channel_blue_D );
+
+                for (i32 i = 0; i < 32; i += 1)
+                {
+                    GPIO_TOGGLE  (led_channel_red_D  );
+                    GPIO_INACTIVE(led_channel_green_D);
+                    GPIO_TOGGLE  (led_channel_blue_D );
+                    FREERTOS_delay_ms(25);
+                }
+
+                FREERTOS_delay_ms(1'000);
+
+                goto REINITIALIZE;
+
+            } break;
+
+
+
+            // Something went wrong trying to mount the file-system;
+            // best thing we can do is format the card and hope it'll work out.
+
+            case FileSystemReinitResult_invalid_filesystem:
+            case FileSystemReinitResult_no_more_space_for_new_file:
+            case FileSystemReinitResult_fatfs_internal_error:
+            case FileSystemReinitResult_bug:
+            default:
+            {
+
+                GPIO_INACTIVE(led_channel_red_D  );
+                GPIO_INACTIVE(led_channel_green_D);
+                GPIO_INACTIVE(led_channel_blue_D );
+
+                for (i32 i = 0; i < 128; i += 1)
+                {
+                    GPIO_TOGGLE(led_channel_red_D  );
+                    GPIO_TOGGLE(led_channel_green_D);
+                    GPIO_TOGGLE(led_channel_blue_D );
+                    FREERTOS_delay_ms(25);
+                }
+
+                FREERTOS_delay_ms(1'000);
+
+                completely_wipe_filesystem = true;
+
+                goto REINITIALIZE;
+
+            } break;
+
+        }
+
+
+
+        // Alright, file-system is ready. Let's start logging!
+
+        u32 most_recent_heartbeat_timestamp_us = 0;
+
+        while (true)
+        {
+
+            // Make the log entry.
+
+            i32 log_entry_length =
+                snprintf_
+                (
+                    (char*) working_sectors,
+                    sizeof(working_sectors),
+                    "[%lu us]" "\n"
+                    "Meow"     "\n"
+                    "\n",
+                    TIMEKEEPING_microseconds()
+                );
+
+
+
+            // Formatting error, if for some reason.
+
+            if (log_entry_length <= -1)
+            {
+                sus;
+                log_entry_length = 0;
+            }
+
+
+
+            // Log entry too long; just truncate.
+
+            if (log_entry_length > sizeof(working_sectors))
+            {
+                sus;
+                log_entry_length = sizeof(working_sectors);
+            }
+
+
+
+            // The remaining space in the log buffer will be used for the divider.
+
+            memset
+            (
+                (u8*) working_sectors + log_entry_length,
+                '.',
+                (u32) (sizeof(working_sectors) - log_entry_length)
+            );
+
+            working_sectors[countof(working_sectors) - 1].bytes[sizeof(struct Sector) - 2] = '\n'; // Don't care if this stamps over the string.
+            working_sectors[countof(working_sectors) - 1].bytes[sizeof(struct Sector) - 1] = '\n'; // "
+
+
+
+            // Save onto SD card for us to verify the vehicle
+            // is operating as intended after sequence testing.
+
+            enum FileSystemSaveResult save_result =
+                FILESYSTEM_save
+                (
+                    SDHandle_primary,
+                    working_sectors,
+                    countof(working_sectors)
+                );
+
+            switch (save_result)
+            {
+
+
+
+                // Successfully saved the log entry;
+                // periodically indicate that we're logging data too.
+
+                case FileSystemSaveResult_success:
+                {
+                    if (TIMEKEEPING_microseconds() - most_recent_heartbeat_timestamp_us >= 5'000'000)
+                    {
+                        most_recent_heartbeat_timestamp_us = TIMEKEEPING_microseconds();
+                        GPIO_INACTIVE(led_channel_red_D  );
+                        GPIO_TOGGLE  (led_channel_green_D);
+                        GPIO_INACTIVE(led_channel_blue_D );
+                    }
+                } break;
+
+
+
+                // Small error occured; let's just reinitialize the file-system
+                // as quickly as possible to be able to continue logging, if still possible.
+
+                case FileSystemSaveResult_transfer_error:
+                {
+                    goto REINITIALIZE;
+                } break;
+
+
+
+                // Uh oh, let's just wipe the SD card so we can continue logging new data.
+
+                case FileSystemSaveResult_no_more_space_for_data:
+                case FileSystemSaveResult_fatfs_internal_error:
+                {
+                    completely_wipe_filesystem = true;
+                    goto REINITIALIZE;
+                } break;
+
+
+
+                // Something weird happened. Let's just reinitialize
+                // the file-system and hope for the best...
+
+                case FileSystemSaveResult_bug:
+                default:
+                {
+                    goto REINITIALIZE;
+                } break;
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 INTERRUPT_I2Cx_communication(enum I2CSlaveCallbackEvent event, u8* data)
 {
 
