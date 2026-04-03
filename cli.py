4c3b173bdec26f79917e31ee17a877e0d1eaad8a
Phuc X. Doan <phucxdoan@gmail.com>
2026-04-03 14:19:19 -0400

    Cleaned up `parseVideo`.

--- cli.py
+++ cli.py
@@ -34,7 +34,7 @@ except ModuleNotFoundError as error:
 
 # Built-in modules.
 
-import types, pathlib, shutil, subprocess, time, io, struct
+import types, pathlib, shutil, subprocess, time, io, struct, math
 
 
 
@@ -2202,6 +2202,13 @@ def verifyLogs(parameters):
         'type'        : str,
         'default'     : None
     },
+    {
+        'name'        : 'fps',
+        'description' : 'Frame rate of the video.',
+        'type'        : int,
+        'flag_only'   : True,
+        'default'     : 24
+    },
 )
 def parseVideo(parameters):
 
@@ -2216,127 +2223,127 @@ def parseVideo(parameters):
 
 
 
-    import os, sys
     import cv2
-    import numpy as np
-    # include operation systems, OpenCV (decodes JPEG and creates/writes video), numpy so we can convert raw bites into array data for CV
-    # OpenCV is a computer vision library used for image processing, machine learning, and other cv applications
-
-
-    START_TOKEN = b"<TV>"
-    END_TOKEN = b"</TV>"
-    # Binary tokens representing the start and end of JPEG image frames
-    # <TV> [JPEG DATA] </TV>
-
-    FPS = 24  # frame rate
+    import numpy
 
 
 
-    def extract_frames_from_blob(file_path):
-        """
-        This function extracts the JPEG bytes using the TV tokens, attempts 
-        to decode them using OpenCV, and produces valid frames one at a time.
-        """
+    # Lazily parse for image frames.
 
-        # reads entire file and stores raw bytes as blob
-        f = open(file_path, "rb")
+    def frame_generator():
 
-        # initialize frames to 0
-        total_frames = 0
-        recovered_frames = 0
-
-        blob = b''
+        file_handle    = open(input_file_path, 'rb')
+        working_window = b''
 
         while True:
 
-            if len(blob) < 100 * 1024:
-                blob += f.read(100 * 1024)
 
-            start = blob.find(START_TOKEN)
-            if start == -1:
+
+            # If the binary blob is massive, we load it in chunks at a time
+            # and process it as so. This breaks if the image frame is larger
+            # than the specified chunk here, but for our situation, this is
+            # good enough.
+
+            if len(working_window) < 1024 * 1024:
+                working_window += file_handle.read(1024 * 1024)
+
+
+
+            # Find the start and end of the image frame, if any.
+
+            if (start := working_window.find(TV_TOKEN.START)) == -1:
                 break
-            # finds start token, if none break
 
-            end = blob.find(END_TOKEN, start)
-            if end == -1:
+            if (end := working_window.find(TV_TOKEN.END, start)) == -1:
                 break
-            # finds end token, if none break
-
-            #if start and end token found, begin looping through data
-
-            jpeg_data = blob[start + len(START_TOKEN):end]
-            # takes all the raw data between start and end token, allocates total frames
-
-            total_frames += 1
-
-            # Basic JPEG sanity check, makes sure the file begins with FF D8
-            if not jpeg_data.startswith(b"\xFF\xD8"):
-                blob = blob[end + len(END_TOKEN):]
-                continue
-
-            # Attempt to decode
-            # np.frombuffer() converts raw bytes into a NumPy array called image_array
-            # vc2.imdecode() translates image_array into a JPEG image
-            try:
-                image_array = np.frombuffer(jpeg_data, dtype=np.uint8)
-                frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-            # if frame is corrupted, skip it
-                if frame is None:
-                    raise ValueError("Decode failed")
-
-            # adds to total recovered/valid frames
-                recovered_frames += 1
-                yield frame
-
-            except Exception:
-                # corrupted frame? skip
-                pass
-
-            blob = blob[end + len(END_TOKEN):]
-
-        print(f"Total frames found: {total_frames}")
-        print(f"Valid frames recovered: {recovered_frames}")
 
 
-    def write_video(frames, output_path, fps):
-        """
-        This function stitches frames into a single MP4 video.
-        """
 
-        #search for valid frames
-        first_frame = next(frames, None)
-        if first_frame is None:
-            print("No valid frames found.")
-            return
+            # Try decoding the image.
 
-        # Define the codec, heigh and width and create VideoWriter object
-        height, width, _ = first_frame.shape
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+            image_array = numpy.frombuffer(
+                working_window[start + len(TV_TOKEN.START) : end],
+                dtype = numpy.uint8
+            )
 
-        writer.write(first_frame)
+            frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-        frame_count = 1
+            if frame is not None:
+                yield file_handle.tell(), frame
 
-        for frame in frames:
-            # print(frame.shape), prints size of each frame before reframing
-            if frame.shape[0] != height or frame.shape[1] != width:
-                frame = cv2.resize(frame, (width, height))
-                # rewrites all frames to be the same size
-                # To Note: it resizes based on the height and width of the first frame
 
-            writer.write(frame)
-            frame_count += 1
 
-        writer.release()
-        print(f"Video written: {output_path}")
-        print(f"Frames written: {frame_count}")
+            # Move onto next frame.
 
-        # output mp4
-    if __name__ == "__main__":
-        frame_generator = extract_frames_from_blob(input_file_path)
-        write_video(frame_generator, output_file_path, FPS)
+            working_window = working_window[end + len(TV_TOKEN.END):]
+
+
+
+    # Have the video's settings be based on the first frame.
+
+    frames = frame_generator()
+
+    _, first_frame = next(frames, (None, None))
+
+    if first_frame is None:
+
+        pxd.pxd_logger.warning(
+            f'No valid frames found in {repr(input_file_path.as_posix())}.'
+        )
+
+        return
+
+    frame_count                  = 1
+    video_height, video_width, _ = first_frame.shape
+
+    writer = cv2.VideoWriter(
+        output_file_path,
+        cv2.VideoWriter_fourcc(*'mp4v'),
+        parameters.fps,
+        (video_width, video_height)
+    )
+
+    writer.write(first_frame)
+
+
+
+    # Process the rest of the frames in the file.
+
+    input_file_size = input_file_path.stat().st_size
+
+    for input_file_cursor_position, frame in frames:
+
+        frame_count                  += 1
+        frame_height, frame_width, _  = frame.shape
+
+        if (frame_width, frame_height) != (video_width, video_height):
+            frame = cv2.resize(frame, (video_width, video_height))
+
+        writer.write(frame)
+
+        if frame_count % 100 == 0:
+
+            pxd.pxd_logger.info(
+                f'Processing {input_file_cursor_position / input_file_size * 100 :.2f}%...'
+            )
+
+
+
+    # Done processing!
+
+    writer.release()
+
+    output_file_size = output_file_path.stat().st_size
+    duration         = frame_count / parameters.fps
+
+    pxd.pxd_logger.info(
+        f'Video written to {repr(output_file_path.resolve().as_posix())}.'                                      '\n'
+        f'Input file size            : {input_file_size  :,} bytes.'                                            '\n'
+        f'Output file size           : {output_file_size :,} bytes.'                                            '\n'
+        f'Frame count                : {repr(frame_count)}.'                                                    '\n'
+        f'Duration                   : {repr(math.floor(duration // 60))}m {repr(math.floor(duration % 60))}s.' '\n'
+        f'Estimated write throughput : {round(input_file_size / duration / 1024) :,} KiB/s.'                    '\n'
+    )
 
 
 
